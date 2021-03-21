400d75e1857c398593d055142494c83ab780c6cb
Petr B <Sgt.Petrov@gmail.com>
2021-03-21 13:21:54 +0100

    Post generation cleanup (#1)
    All files and folders used only for generating the project from template removed.
    Settings of the project updated.

--- Tests/Unit/UnitTests.swift
+++ Tests/Unit/UnitTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import ProjectTemplate
+@testable import MastersThesisIOS
 
 class ProjectTemplateTests: XCTestCase {
 
@@ -23,13 +23,14 @@ class ProjectTemplateTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
