5c8b1e1dd2d7a9c86dd904af723699535f9d3c24
Petr B <Sgt.Petrov@gmail.com>
2021-03-22 10:52:15 +0100

    Small settings change

--- MastersThesisIOS/Extensions/String.swift
+++ MastersThesisIOS/Extensions/String.swift
@@ -3,7 +3,7 @@
 //  MastersThesisIOS
 //
 //  Created by Petr Budík on 21/03/2021.
-//  Copyright © 2021 Ackee, s.r.o. All rights reserved.
+//  Copyright © 2021 Petr Budík. All rights reserved.
 //
 
 import Foundation
---
+++ MastersThesisIOS/Model/RealmData/AnimalData.swift
@@ -0,0 +1,14 @@
+////
+////  AnimalData.swift
+////  MastersThesisIOS
+////
+////  Created by Petr Budík on 22/03/2021.
+////  Copyright © 2021 Petr Budík. All rights reserved.
+////
+//
+//import Foundation
+//
+//class AnimalData {
+//    public let _id: Int64
+//    public let name
+//}
