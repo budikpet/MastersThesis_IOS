d14edd94ca3fc5d9a404882cf0405733629a7d6c
Petr B <Sgt.Petrov@gmail.com>
2021-04-28 10:51:39 +0200

    Cleanup

--- Tests/Unit/Utils/TestDependency.swift
+++ Tests/Unit/Utils/TestDependency.swift
@@ -4,32 +4,20 @@ import CoreLocation
 @testable import MastersThesisIOS
 
 /// Container for all app dependencies
-final class TestAppDependency: HasBaseAPIDependecies, HasCredentialsDependencies, HasManagerDependencies, HasAPIDependencies {
+final class TestAppDependency: HasBaseAPIDependecies, HasManagerDependencies, HasAPIDependencies {
     lazy var network: Networking = Network()
-    lazy var authHandler: AuthHandling = AuthHandler()
     lazy var realm: Realm = self.createEmptyRealm()
 
-    lazy var credentialsProvider: CredentialsProvider = UserDefaults.credentials
-    lazy var credentialsStore: CredentialsStore = UserDefaults.credentials
-
     lazy var jsonAPI: JSONAPIServicing = JSONAPIService(dependencies: self)
-    lazy var authJSONAPI: JSONAPIServicing = AuthenticatedJSONAPIService(dependencies: self)
-    lazy var pushAPI: PushAPIServicing = PushAPIService(dependencies: self)
-    lazy var exampleAPI: ExampleAPIServicing = ExampleAPIService(dependencies: self)
     lazy var zooAPI: ZooAPIServicing = ZooAPIService(dependencies: self)
 
     lazy var fetcher: Fetcher = FirebaseFetcher(key: "min_version")
-    lazy var firebasePushObserver: FirebasePushObserving = FirebasePushObserver(dependencies: self)
 
-    lazy var pushManager: PushManaging = PushManager(dependencies: self)
-    lazy var userManager: UserManaging = UserManager()
-    lazy var versionUpdateManager: VersionUpdateManaging = VersionUpdateManager(dependencies: self)
     lazy var realmDBManager: RealmDBManaging = RealmDBManager(dependencies: self)
     lazy var zooNavigationService: ZooNavigationServicing = ZooNavigationService(dependencies: self)
     lazy var locationManager: CLLocationManager = {
         let manager = CLLocationManager()
         manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-        manager.requestWhenInUseAuthorization()
 
         return manager
     }()
