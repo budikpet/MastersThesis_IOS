6fc49a7cfc3ed4c5d6e333fba72cd7148dac0619
Petr B <Sgt.Petrov@gmail.com>
2021-04-28 10:21:45 +0200

    Cleanup

--- MastersThesisIOS/Services/DI/AppDependency.swift
+++ MastersThesisIOS/Services/DI/AppDependency.swift
@@ -2,31 +2,23 @@ import Foundation
 import RealmSwift
 import CoreLocation
 
-typealias HasBaseAPIDependecies = HasNetwork & HasJSONAPI & HasAuthenticatedJSONAPI & HasAuthHandler & HasRealm
-typealias HasAPIDependencies = HasPushAPI & HasFetcher & HasExampleAPI & HasZooAPI
-typealias HasCredentialsDependencies = HasCredentialsProvider & HasCredentialsStore
-typealias HasManagerDependencies = HasPushManager & HasUserManager & HasFirebasePushObserver & HasVersionUpdateManager & HasRealmDBManager & HasLocationManager & HasZooNavigationService
+typealias HasBaseAPIDependecies = HasNetwork & HasJSONAPI & HasRealm
+typealias HasAPIDependencies = HasPushAPI & HasFetcher & HasZooAPI
+typealias HasManagerDependencies = HasPushManager & HasFirebasePushObserver & HasVersionUpdateManager & HasRealmDBManager & HasLocationManager & HasZooNavigationService
 
 /// Container for all app dependencies
-final class AppDependency: HasBaseAPIDependecies, HasCredentialsDependencies, HasManagerDependencies, HasAPIDependencies {
+final class AppDependency: HasBaseAPIDependecies, HasManagerDependencies, HasAPIDependencies {
     lazy var network: Networking = Network()
-    lazy var authHandler: AuthHandling = AuthHandler()
     lazy var realm: Realm = self.initRealm()
 
-    lazy var credentialsProvider: CredentialsProvider = UserDefaults.credentials
-    lazy var credentialsStore: CredentialsStore = UserDefaults.credentials
-
     lazy var jsonAPI: JSONAPIServicing = JSONAPIService(dependencies: self)
-    lazy var authJSONAPI: JSONAPIServicing = AuthenticatedJSONAPIService(dependencies: self)
     lazy var pushAPI: PushAPIServicing = PushAPIService(dependencies: self)
-    lazy var exampleAPI: ExampleAPIServicing = ExampleAPIService(dependencies: self)
     lazy var zooAPI: ZooAPIServicing = ZooAPIService(dependencies: self)
 
     lazy var fetcher: Fetcher = FirebaseFetcher(key: "min_version")
     lazy var firebasePushObserver: FirebasePushObserving = FirebasePushObserver(dependencies: self)
 
     lazy var pushManager: PushManaging = PushManager(dependencies: self)
-    lazy var userManager: UserManaging = UserManager()
     lazy var versionUpdateManager: VersionUpdateManaging = VersionUpdateManager(dependencies: self)
     lazy var realmDBManager: RealmDBManaging = RealmDBManager(dependencies: self)
     lazy var zooNavigationService: ZooNavigationServicing = ZooNavigationService(dependencies: self)
